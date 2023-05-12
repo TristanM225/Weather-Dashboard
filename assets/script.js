2214faaef51d9a205e1722d67dd14bb5af172f92
TristanM225 <TristanM225@gmail.com>
2023-05-11 22:19:33 -0700

    added basic weather search function

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+
+
+
+var apiKey = '4078f70a829ed0138a18ddaadd787e76';
+
+// here we are grabbing the button by the ID 'search'
+var searchButton = document.getElementById('search');
+
+// when we click the search button the searchWeather function will be activated
+searchButton.addEventListener('click', searchWeather);
+
+function searchWeather() {
+
+// we are scanning the document for the city ID which is in the input.
+  var city = document.getElementById('city').value;
+  // here we take the above variable to add it to the url to create our search
+  var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      var weatherElement = document.getElementById('weather');
+      weatherElement.innerHTML = `
+        <h2>${data.name}</h2>
+        <p>Temperature: ${data.main.temp} °C</p>
+        <p>Description: ${data.weather[0].description}</p>
+      `;
+    })
+    .catch(error => {
+      console.error('Error fetching weather data:', error);
+    });
+}
